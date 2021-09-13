af6c84e75bcd6ae055b9de4534f40db79ae01824
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-09-13 13:30:09 +0600

    commit

--- js/app.js
+++ js/app.js
@@ -11,13 +11,15 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="single-product ">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h5>Average Rating: ${product.rating.rate} <i class="fa fa-star yellow"></i></h5>
+      <h5>${product.rating.count} People rated this product</h5><br>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
